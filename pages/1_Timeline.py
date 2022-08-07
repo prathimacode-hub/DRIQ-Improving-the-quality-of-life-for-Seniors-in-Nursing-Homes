011b5a62dc0f27a4663e9dafafa1759e525a9fa0
Prathima Kadari <kadariprathima4@gmail.com>
2022-08-07 15:13:03 +0530

    Create 1_Timeline.py

---
+++ pages/1_Timeline.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.sidebar.header("Timeline")
+
+st.subheader("Overview")
+
+st.markdown(f'Timeline from the Start of the Week: ')
+
+st.markdown('<h3 style=color:#4169e1;">Week 1-2</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+* 
+
+""", unsafe_allow_html=True)
