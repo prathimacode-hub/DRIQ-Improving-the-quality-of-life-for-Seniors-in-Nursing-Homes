7cf3893390fa691523e0a6d54111029ae5c43e3b
Prathima Kadari <kadariprathima4@gmail.com>
2022-08-07 15:09:50 +0530

    Create 2_Progress_Made.py

---
+++ pages/2_Progress_Made.py
@@ -0,0 +1,89 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.sidebar.header("Tasks Progress")
+
+#st.markdown(f'Task 1:')
+
+st.markdown('<h2 style=color:#4169e1;">Task 1 - Exploratory Data Analysis</h2>', unsafe_allow_html=True)
+
+st.markdown('<h3 style=color:#4169e1;">Progress Made</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+
+
+""", unsafe_allow_html=True)
+
+st.markdown('<h3 style=color:#4169e1;">Next Steps</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+
+
+""", unsafe_allow_html=True)
+
+
+st.markdown('<h3 style=color:#4169e1;">References</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+
+   
+""", unsafe_allow_html=True)
+
+
+#st.markdown(f'Task 2:')
+
+st.markdown('<h2 style=color:#4169e1;">Task 2 - Modelling</h2>', unsafe_allow_html=True)
+
+st.markdown('<h3 style=color:#4169e1;">Progress Made</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+
+
+""", unsafe_allow_html=True)
+
+st.markdown('<h3 style=color:#4169e1;">Next Steps</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+
+
+""", unsafe_allow_html=True)
+
+st.markdown('<h3 style=color:#4169e1;">References</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+
+  
+""", unsafe_allow_html=True)
+
+
+#st.markdown(f'Task 3:')
+
+st.markdown('<h2 style=color:#4169e1;">Task 3 - Deployment</h2>', unsafe_allow_html=True)
+
+st.markdown('<h3 style=color:#4169e1;">Progress Made</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+
+
+""", unsafe_allow_html=True)
+
+st.markdown('<h3 style=color:#4169e1;">Next Steps</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+
+
+""", unsafe_allow_html=True)
+
+
+st.markdown('<h3 style=color:#4169e1;">References</h1>', unsafe_allow_html=True)
+
+st.markdown(""" 
+
+  
+""", unsafe_allow_html=True)
+
