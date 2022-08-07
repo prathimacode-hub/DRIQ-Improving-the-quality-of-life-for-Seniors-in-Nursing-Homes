8a797d76f5ee114c25c9ff10d2e3306821e65949
Prathima Kadari <kadariprathima4@gmail.com>
2022-08-07 15:11:18 +0530

    Create 6_Team.py

---
+++ pages/6_Team.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(page_title="Team")
+
+st.sidebar.header("Team")
+
+st.subheader('Collaborators')
+
+st.markdown("""
+
+""")
+
+st.subheader('Product Lead')
+
+st.markdown('[]()')
