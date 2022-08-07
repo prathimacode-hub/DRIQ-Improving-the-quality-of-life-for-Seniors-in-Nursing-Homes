b1ca610f09cb5de5c9defab9c72eb17efe042006
Prathima Kadari <kadariprathima4@gmail.com>
2022-08-07 15:19:14 +0530

    Create 4_Modelling.py

---
+++ pages/4_Modelling.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Project Outcome",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.sidebar.header("Project Outcome")
+
+st.subheader("Modeling Process")
+st.markdown("""
+*  
+""")
