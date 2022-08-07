738b0a23f721e5cf9de981e88fa60011b491e1f9
Prathima Kadari <kadariprathima4@gmail.com>
2022-08-07 15:17:04 +0530

    Create 3_Exploratory_Data_Analysis.py

---
+++ pages/3_Exploratory_Data_Analysis.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import streamlit as st
+import chart_studio.plotly as py
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+PAGE_TITLE = "Exploratory Data Analysis"
+st.sidebar.header("Visualizations")
+
+import plotly_express as px
+
+st.markdown("<h2 style='text-align: center; color: #4169e1;margin-top:-50px;font-weight:bold'>OMDENA-DRIQ HEALTH</h2>", unsafe_allow_html=True)
